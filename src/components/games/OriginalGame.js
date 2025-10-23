a01d8f17c3ee8c299f6169b0d0a2abf22822f5e9
phandoko <petrus.handoko@gmail.com>
2025-10-23 10:42:50 -0700

    fixed the grid problem

--- src/components/games/OriginalGame.js
+++ src/components/games/OriginalGame.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import '../../styles/ClassicGame.css';
+import '../../styles/OriginalGame.css';
 import { calculateWinner, findForcedMove } from '../../utils/gameLogic';
 
 
@@ -203,29 +203,31 @@ function OriginalGame() {
 
   return (
     <>
-      <div>
-        <span align="left" valign="top">
+      <div className="game-container">
+        <div className="sidebar">
           <AboutPage/>
           <Profile/>
-        </span>
-        <span align="left" valign="top">
-			<h2> Tic tac Toe</h2>
-          <div className="board-row">
-            <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
-            <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
-            <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
+        </div>
+        <div className="main-content">
+          <h2>Tic tac Toe</h2>
+          <div className="game-board">
+            <div className="board-row">
+              <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+              <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
+              <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
+            </div>
+            <div className="board-row">
+              <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+              <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+              <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
+            </div>
+            <div className="board-row">
+              <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+              <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+              <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
+            </div>
           </div>
-          <div className="board-row">
-            <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
-            <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
-            <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
-          </div>
-          <div className="board-row">
-            <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
-            <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
-            <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
-          </div>
-        </span>
+        </div>
       </div>
       <br/>
       <div>
